759ccbb4f3d21cebb1f1e14c8fc89f98d237c64a
Admin <olegtranslator@gmail.com>
2016-03-20 09:50:32 +0100

    first initial commit

---
+++ grails-app/controllers/rewards/InventoryController.groovy
@@ -0,0 +1,15 @@
+package rewards
+
+class InventoryController {
+
+    def index() {
+
+        render "Here is a list of everything."
+
+    }
+
+    def remove() {
+
+        render "You have one more."
+    }
+}
